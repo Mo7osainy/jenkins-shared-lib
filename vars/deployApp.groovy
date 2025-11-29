cbcfd7c49ce6a7a81669eb285b2e4a833f188d9d
Mo_7osainy <mohamedalhosainy2323@gmail.com>
2025-11-29 21:53:36 +0200

    Update deployApp.groovy

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -1,11 +1,10 @@
 def call(Map config = [:]) {
     def imageName = config.image ?: 'mo7osainy/java-app'
-    def tag       = config.tag ?: "${env.BUILD_NUMBER}"
     def port      = config.port ?: 8080
 
     echo "Deploying ${imageName}:${tag} on port ${port}"
 
     sh 'docker rm -f java-app || true'
-    sh "docker run -d --name java-app -p ${port}:8080 ${imageName}:${tag}"
+    sh "docker run -d --name java-app -p ${port}:8080 ${imageName}:13"
 }
 
